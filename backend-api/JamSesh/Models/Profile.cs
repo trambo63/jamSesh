fa45f0c48a3a5ec60cd34bc7fd050f6d8345b0c7
Tom <tomrambo95@yahoo.com>
2020-07-28 11:13:09 -0400

    added jam/profile model

---
+++ backend-api/JamSesh/Models/Profile.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace JamSesh.Models
+{
+    public class Profile
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Location { get; set; }
+        public string Instruments { get; set; }
+        public string Image { get; set; }
+        public string Description { get; set; }
+        public List<Jam> JamsAttending { get; set; }
+
+    }
+}
