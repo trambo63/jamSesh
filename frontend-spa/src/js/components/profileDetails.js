865ec01e4cff4969954295b0dd8af39cf96513e1
Tom <tomrambo95@yahoo.com>
2020-08-04 11:52:55 -0400

    added styling to profile details and jam details page

--- frontend-spa/src/js/components/profileDetails.js
+++ frontend-spa/src/js/components/profileDetails.js
@@ -1,14 +1,15 @@
 export default function ProfileDetails(profile) {
     return `
-    <li class="workoutdetails-collection"/li>
+    <section class="workout-style">
+    <div class="workoutdetails-collection"/div>
     <h2>${profile.name}</h2>  
-    <li><a class="profiledetails__name" id="${profile.id}" href="#"> <img src="images/${profile.image}" alt="${profile.image}" width="200" height="200"> ${profile.name}</a></li>
-    <li>${profile.location}</li>
-    <li>${profile.instruments}</li>
-    <li>${profile.description}</li>
+    <p><a class="profiledetails__name" id="${profile.id}" href="#"> <img src="images/${profile.image}" alt="${profile.image}" width="200" height="200"> ${profile.name}</a></p>
+    <p>Location: ${profile.location}</p>
+    <p>Instruments: ${profile.instruments}</p>
+    <p>About: ${profile.description}</p>
+    </section>
     <button class="delete-profile__button" id="${profile.id}" value="${profile.id}">Delete Profile</button>                   
     <button class="profile__edit_button" id="${profile.id}" value="${profile.id}">Edit Profile</button>                   
     
-    
     `
-}
+}       
