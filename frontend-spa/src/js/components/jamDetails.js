4efda346a8c9b47db71524818b720eeff47d0fbc
Tom <tomrambo95@yahoo.com>
2020-08-04 11:30:55 -0400

    date time display readonly

--- frontend-spa/src/js/components/jamDetails.js
+++ frontend-spa/src/js/components/jamDetails.js
@@ -5,7 +5,7 @@ export default function JamDetails(jam) {
     <li><a class="jamdetails__name" id="${jam.id}" href="#"> <img src="images/${jam.image}" alt="${jam.image}" width="200" height="200"> ${jam.name}</a></li>
     <li>${jam.location}</li>
     <li>${jam.description}</li>
-    <li>${jam.eventDate}</li>
+    <li> <input type="datetime-local" value="${jam.eventDate}" readonly="readonly"></li>
     <li>${jam.maxNumberOfAttendees}</li>
 
     <button class="delete-jam__button" id="${jam.id}" value="${jam.id}">Delete Jam</button>                   
