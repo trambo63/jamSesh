a35bf1f54c8c465d50ab505f982fb1f4c5b3f97a
Tom <tomrambo95@yahoo.com>
2020-08-04 20:01:16 -0400

    wip map

--- frontend-spa/src/js/components/map.js
+++ frontend-spa/src/js/components/map.js
@@ -1,8 +1,19 @@
 export default function map(){
     return `
-            
-        
-              
+    
+    <p>
+	    <strong>Address:</strong>
+	    <br>205 Gould Ave
+	    <br>Bedford Ohio 44146
+    </p>
+
+    <p>
+	    <strong>Phone:</strong>
+	    <br>2163758452
+    </p> 
+      
+    <p><iframe src="https://www.google.com/maps/place/205+Gould+Ave,+Bedford,+OH+44146/@41.4021385,-81.5476259,17z/data=!3m1!4b1!4m5!3m4!1s0x8830e25b05ad3fe3:0xcc91adee10e59e2e!8m2!3d41.4021385!4d-81.5454372"
+    width="600" height="450" frameborder="0" style="border:0" allowfullscreen></iframe></p>
 
 `
 }
