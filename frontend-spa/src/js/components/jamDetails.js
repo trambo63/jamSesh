9eaa8a80d9b463cd8ee365ecefbe1566de63b834
Tom <tomrambo95@yahoo.com>
2020-08-06 12:03:19 -0400

    wip remove jamAttendee/update jam button bug

--- frontend-spa/src/js/components/jamDetails.js
+++ frontend-spa/src/js/components/jamDetails.js
@@ -6,7 +6,7 @@ export default function JamDetails(jam) {
     <li><a class="jamdetails__name" id="${jam.jamId}" href="#"> <img src="images/${jam.image}" alt="${jam.image}" width="200" height="200"> ${jam.name}</a></li>
     <li>${jam.location}</li>
     <li>${jam.description}</li>
-    <li>${jam.eventDate}</li>
+    <li><input type="datetime-local" readonly="readonly" value="${jam.eventDate}"</li>
     <li>${jam.maxNumberOfAttendees}</li>
 
     <button class="delete-jam__button" id="${jam.jamId}" value="${jam.jamId}">Delete Jam</button>                   
