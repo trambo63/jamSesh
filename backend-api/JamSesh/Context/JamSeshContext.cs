2d3b3551c26876185cb384a3d4e3e5055f232586
Corey T Cox <coxcorey19@gmail.com>
2020-08-07 14:04:55 -0400

    adjusted images

--- backend-api/JamSesh/Context/JamSeshContext.cs
+++ backend-api/JamSesh/Context/JamSeshContext.cs
@@ -77,7 +77,7 @@ namespace JamSesh.Context
                 Image = "DefaultProfilePhoto.jpg",
                 Instruments = "Bass, Drums",
                 Description = "New to the area, and I just want to get together with some people on the weekends to play some tunes",
-                Location = "26635 Brookpark Rd, North Olmsted, OH 44070",
+                Location = "North Olmsted, OH",
                 Password = "Welcome"
             },
 
@@ -88,7 +88,7 @@ namespace JamSesh.Context
                 Image = "DefaultProfilePhoto.jpg",
                 Instruments = "Keys, Acoustic Guitar, Vocals",
                 Description = "I like playing jazz standards in small trios",
-                Location = "7705 W Ridgewood Dr, Parma, OH 44129",
+                Location = "Parma, OH",
                 Password = "Welcome"
             },
 
@@ -99,27 +99,47 @@ namespace JamSesh.Context
                 Image = "DefaultProfilePhoto.jpg",
                 Instruments = "Mandalin, Guitar, Bass, Drums",
                 Description = "I've got more instruments than just the ones I play. I'm usually available on the weekends. LET'S JAM",
-                Location = "7415 Memphis Ave, Brooklyn, OH 44144",
+                Location = "Brooklyn, OH",
                 Password = "Welcome"
             }
 
             );
 
             modelbuilder.Entity<Jam>().HasData(
-                new Jam
-                {
-                    JamId = 1,
-                    Name = "Acoustic Rock in the Park",
-                    Location = "6500 Cleveland Memorial Shoreway, Cleveland, OH 44102",
-                    Description = "Need 2 more people to play some 80s Rock at Edgewater Park. Could use vocals and bass/guitar.",
-                    EventDate = DateTime.Now,
-                    MaxNumberOfAttendees = 3,
-                    Image = "JamIcon.jpg"
+            new Jam
+            {
+                JamId = 1,
+                Name = "High Voltage Acoustic Rock",
+                Location = "333 Foundry St, Medina, OH 44256",
+                Description = "Need 2 more people to play some 80s Rock at Edgewater Park. Could use vocals and bass/guitar.",
+                EventDate = DateTime.Now,
+                MaxNumberOfAttendees = 3,
+                Image = "JamIcon.jpg"
+            },
 
+            new Jam
+            {
+                JamId = 2,
+                Name = "Castle Noel Big Band",
+                Location = "260 S Court St, Medina, OH 44256",
+                Description = "I need a lot of people and a pretty decent range of intruments so we can meet at castle noel and play Duke Ellington and Count Basie songs.",
+                EventDate = DateTime.Now,
+                MaxNumberOfAttendees = 7,
+                Image = "JamIcon.jpg"
+            },
 
-                }
+            new Jam
+            {
+                JamId = 3,
+                Name = "Church Lot Duet",
+                Location = "606 E Washington St, Medina, OH 44256",
+                Description = "I need a drummer to play along with me while I play acoustic guitar and sing. Looking to play some popular gospel standards at my church this Sunday!",
+                EventDate = DateTime.Now,
+                MaxNumberOfAttendees = 2,
+                Image = "JamIcon.jpg"
+            }
 
-                );
+            );
 
 
             modelbuilder.Entity<ProfileJam>().HasData(
--- frontend-spa/src/js/components/listJams.js
+++ frontend-spa/src/js/components/listJams.js
@@ -10,10 +10,9 @@ export default function listJams(jams){
                     <li><div>${jam.description}</div>
                     <li><h3>Where: ${jam.location}</h3>
                     <li><h3>When: <input type="datetime-local" readonly="readonly" value="${jam.eventDate}"></h3>
+                    <div id="jamIcon">
                     <button class="jam-details__button" id="${jam.jamId}">Jam Details</button>
-
                     <img class="jam__name" src="images/jamIcon.png" alt="${jam.image}" width="25px" height="25px"></img>
-                </a>
             </li>
         </section>
        
