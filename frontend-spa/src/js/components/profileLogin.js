0fe184d26de84a990c076f8ba7ccdf1d021e5990
Corey T Cox <coxcorey19@gmail.com>
2020-08-12 11:21:21 -0400

    added side images. WIP adding welcome text

--- frontend-spa/src/js/components/profileLogin.js
+++ frontend-spa/src/js/components/profileLogin.js
@@ -1,9 +1,20 @@
 export default function ProfileLogin() {
     return `
+<section class="Logon">
 <h3>Username</h3>  
 <input class="logon-profile__name" type="text" placeholder="Enter your username">
 <h3>Password</h3>
 <input class="logon-profile__password" type="text" placeholder="Enter your password">
 <button class="logon-profile__submit">Logon</button>
+</section>
+<div class="SideImages">
+<div id="leftImage">
+<img src="images/RedSpaceImage1.png" alt="guitarist" height="450" width="250">
+</div>
+<div id="rightImage">
+<img src="images/RedSpaceImage2.png" alt="drummer" height="450" width="250">
+</div>
+</div>
+
     `
 }
